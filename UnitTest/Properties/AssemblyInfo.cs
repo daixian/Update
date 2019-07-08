afc33288b47ada94bad0ccbd767c3c61bed4cb5d
daixian <amano_tooko@qq.com>
2019-07-08 18:50:25 +0800

    换了个Flurl库

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a63f9a7-f253-450f-bf1d-ffa99c4ab0f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flurl;
+using Flurl.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SemaphoreSlim semaphore = new SemaphoreSlim(0);
+            Task.Run(async () =>
+            {
+                var msg = await "http://mr.xuexuesoft.com:8012/hi".GetAsync();
+                string str = msg.ToString();
+                semaphore.Release();
+            });
+
+            semaphore.Wait();
+        }
+    }
+}
